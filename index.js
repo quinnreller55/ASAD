19e26ef924204764b0145230e8387b2455048242
Quinn Reller <61479103+quinnreller55@users.noreply.github.com>
2020-03-31 12:15:29 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const webroot = __dirname + '/public'
-
-app.use('/', express.static(webroot))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
